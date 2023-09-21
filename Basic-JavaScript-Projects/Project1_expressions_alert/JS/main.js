89ba1014b52b8ffa704bb92faac7a87fd9abe76b
ltang1998 <145419345+ltang1998@users.noreply.github.com>
2023-09-20 22:21:28 -0700

    complete comment assignment

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,11 @@
 // alert("Hello, World!!!!");
 // window.alert("Hello, World!!!! by window.alert");
 
+// Welcome window pop up
 var welcome = "How are you doing?";
 window.alert(welcome);
 
+// Write statement on the web page
 document.write("Hello, World!!!! by document.write.<br>");
 
 var statement = "Hey everyone!<br>";
