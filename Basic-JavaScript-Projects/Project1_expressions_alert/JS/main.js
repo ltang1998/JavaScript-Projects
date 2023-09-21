8f0a2a34b6604361efa7cfe795d78aaa22081fdc
ltang1998 <145419345+ltang1998@users.noreply.github.com>
2023-09-20 18:48:46 -0700

    complete escape character assignment

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,10 +1,13 @@
 // alert("Hello, World!!!!");
 // window.alert("Hello, World!!!! by window.alert");
 
-var welcome = "How are you doing?"
+var welcome = "How are you doing?";
 window.alert(welcome);
 
-document.write("Hello, World!!!! by document.write. ");
+document.write("Hello, World!!!! by document.write.<br>");
 
-var statement = "Hey everyone!";
+var statement = "Hey everyone!<br>";
 document.write(statement);
+
+var statementB = "I said, \"Good morning boys! I don't like sushi.\"";
+document.write(statementB);
