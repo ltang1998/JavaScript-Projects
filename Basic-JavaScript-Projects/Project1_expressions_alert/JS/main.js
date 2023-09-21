3b395651c20d815b6a803ac20405f3272e84b82b
ltang1998 <145419345+ltang1998@users.noreply.github.com>
2023-09-20 18:32:31 -0700

    update code

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,3 @@
-// alert("Hello, World!!!!")
-window.alert("Hello, World!!!! by window.alert")
-document.write("Hello, World!!!! by document.write")
+// alert("Hello, World!!!!");
+window.alert("Hello, World!!!! by window.alert");
+document.write("Hello, World!!!! by document.write");
