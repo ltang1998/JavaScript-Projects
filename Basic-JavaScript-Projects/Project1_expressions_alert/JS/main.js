6c04de0678282339e8c85ac879f5104e4fb3af81
ltang1998 <145419345+ltang1998@users.noreply.github.com>
2023-09-20 18:38:53 -0700

    complete variable assignment

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,6 @@
 // alert("Hello, World!!!!");
 window.alert("Hello, World!!!! by window.alert");
-document.write("Hello, World!!!! by document.write");
+document.write("Hello, World!!!! by document.write. ");
+
+var statement = "Hey everyone!";
+document.write(statement);
