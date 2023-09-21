814480c91ae47c739e89ffe807447c3d2b708724
ltang1998 <145419345+ltang1998@users.noreply.github.com>
2023-09-20 18:41:16 -0700

    complete string assignment

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,9 @@
 // alert("Hello, World!!!!");
-window.alert("Hello, World!!!! by window.alert");
+// window.alert("Hello, World!!!! by window.alert");
+
+var welcome = "How are you doing?"
+window.alert(welcome);
+
 document.write("Hello, World!!!! by document.write. ");
 
 var statement = "Hey everyone!";
